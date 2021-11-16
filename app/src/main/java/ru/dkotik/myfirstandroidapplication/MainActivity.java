257a30ba024bb09eb96218dbf0b78ad6a89e08f5
Dmitry Kotik <k-work31@yandex.ru>
2021-11-16 22:25:33 +0300

    Добавлен второй макет. Добавлено несколько обработчиков событий

--- app/src/main/java/ru/dkotik/myfirstandroidapplication/MainActivity.java
+++ app/src/main/java/ru/dkotik/myfirstandroidapplication/MainActivity.java
@@ -2,7 +2,15 @@ package ru.dkotik.myfirstandroidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText input = findViewById(R.id.editTextTextPersonName2);
+        TextView target = findViewById(R.id.textView2);
+        Button btn = findViewById(R.id.button);
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch switcher = findViewById(R.id.switcher);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String inputText = input.getText().toString();
+                target.setText(inputText);
+                input.setText("");
+
+            }
+        });
+
+        switcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    btn.setBackgroundColor(Color.BLACK);
+                } else {
+                    btn.setBackgroundColor(Color.GREEN);
+                }
+            }
+        });
     }
 }
